7197858020083893e28f110b934d2040bc5dfada
wangqishi_w@163.com <131415zxc>
2018-10-29 23:01:09 +0800

    历史轨迹和导航栏

--- CompanyProject/Controllers/HomeController.cs
+++ CompanyProject/Controllers/HomeController.cs
@@ -9,6 +9,8 @@ namespace CompanyProject.Controllers
 {
     public class HomeController : Controller
     {
+        GPSManager _manager = new GPSManager();
+
         public ActionResult Index()
         {
             return View();
@@ -26,12 +28,17 @@ namespace CompanyProject.Controllers
 
         public string GetLocations()
         {
-            GPSManager manager = new GPSManager();
-            var list = manager.GetGpsItems();
+            var list = _manager.GetGpsItems();
 
             string url = "http://www.gps902.net/api/GetMonitor.aspx?ids=356803210132270,353507000002012,353507100003389&mapType=baidu&key=20161222HLXTJDMW730XY";
 
             string json = Common.HttpHelper.Get(url);
+            return Common.Utils.SerializeObject(list);
+        }
+
+        public string GetLocaHis(Guid itemId)
+        {
+            var list = _manager.GetGPSHises(itemId);
             return Common.Utils.SerializeObject(list);
         }
 
