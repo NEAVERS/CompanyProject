2b266999c4afccc3d02756952f7d05f42c0186dd
汪琪诗 <wangqishi_w@163.com>
2018-11-13 08:59:47 +0800

    Update Bll/CheckManager.cs

--- Bll/CheckManager.cs
+++ Bll/CheckManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common;
 using Common.Entities;
 using Dal;
 using Model;
@@ -13,5 +14,14 @@ namespace Bll
     {
         private MyDbContext _context = new MyDbContext();
         private ResponseModel _response = new ResponseModel();
+
+
+
+        public CheckInfo CreateCheckInfo(Guid typeId)
+        {
+            var checkType = _context.CheckTypes.FirstOrDefault(x => x.Id == typeId);
+            var checkSubs = checkType.GetTypeItems();
+
+        }
     }
 }
